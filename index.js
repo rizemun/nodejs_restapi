f1a3010b0d9b57c03d01e706f5e162d6f599cf24
rizemun <rizeman01@gmail.com>
2021-03-23 01:22:38 +0300

    :sparkles: parsing request paths

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ *  Primary file for the API
+ *
+ *
+ *
+ */
+
+// Dependencies
+const http = require('http');
+const url = require('url');
+
+// The server should respond to all requests with a string
+const server = http.createServer(function(req, res) {
+
+  // Get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path of url
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g,'')
+
+  // Send the response
+  res.end('Hello world \n');
+
+  // Log the request path
+  console.log(`Request received on path: ${trimmedPath}`)
+
+})
+
+//Start the server, and have it listen on port 3000
+server.listen(3000,function() {
+  console.log('The server is listening on port 3000 now')
+})
