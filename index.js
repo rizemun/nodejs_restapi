b26ce069dc9f4adae7a21025a9971c55c839c40a
rizemun <rizeman01@gmail.com>
2021-03-24 19:04:59 +0300

    :sparkles: routing requests

--- index.js
+++ index.js
@@ -46,11 +46,63 @@ const server = http.createServer(function (req, res) {
     console.log(`Request received with payload: ${buffer}`)
   });
 
-  // Send the response
-  res.end('Hello world \n');
+  // Choose the handler the request should go to. If no one found choose notFound handler.
+  const chosenHandler = typeof(router[trimmedPath]) !== undefined
+    ? router[trimmedPath]
+    : router.notFound;
+
+  //  Construct data object to send to the handler
+  const data = {
+    trimmedPath,
+    queryStringObject,
+    method,
+    headers,
+    'payload': buffer
+  }
+
+  //  Route the request to the handler specified in the router
+  chosenHandler(data, function(statusCode, payload) {
+    //  Use the status code called back by the handler, or default to 200
+    statusCode = typeof(statusCode) === 'number'
+      ? statusCode
+      : 200;
+
+    // Use the payload called back by the handler, or default to empty object
+    payload = typeof(payload) === 'object'
+      ? payload
+      : {}
+
+    //  Convert the payload to a string
+    const paylodString = JSON.stringify(payload);
+
+    // Return the responce
+    res.writeHead(statusCode);
+    res.end(paylodString);
+
+  })
 })
 
 //Start the server, and have it listen on port 3000
 server.listen(3000, function () {
   console.log('The server is listening on port 3000 now')
 })
+
+// Define the handlers
+const handlers = {};
+
+// Sample handler
+handlers.sample = function(data, callback){
+  //Callback a http status code, and a payload object
+  callback(406, {name:'sample handler'})
+};
+
+//Not found handler
+handlers.notFound = function(data, callback) {
+    callback(404);
+};
+
+
+// Define a request router
+const router = {
+  'sample': handlers.sample
+}
