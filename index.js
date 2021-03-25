d1b8e5c3cba801849e0d6833bc116a690f04b1a6
rizemun <rizeman01@gmail.com>
2021-03-25 23:07:41 +0300

    :sparkles: add users workflow

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ const http = require('http');
 const https = require('https');
 const url = require('url');
 const {StringDecoder} = require('string_decoder')
-const config = require('./config');
 const fs = require('fs')
+const config = require('./lib/config');
+const handlers = require('./lib/handlers')
+const helpers = require('./lib/helpers')
 
 // Instantiate the HTTP server
 const httpServer = http.createServer(function (req, res) {
@@ -37,28 +39,8 @@ httpsServer.listen(config.httpsPort, function () {
   console.log(`The server is listening on port ${config.httpsPort} in ${config.envName} mode now`);
 })
 
-// Define the handlers
-const handlers = {};
-
-// Ping handler
-handlers.ping = function(data, callback){
-  // Callback a http status code, and a payload object
-  callback(200)
-};
-
-// Not found handler
-handlers.notFound = function(data, callback) {
-    callback(404);
-};
-
-
-// Define a request router
-const router = {
-  'ping': handlers.ping
-}
-
 // All the server logic for both the http and https server
-const unifiedServer = function(req, res) {
+const unifiedServer = function (req, res) {
 
   // Get the url and parse it
   const parsedUrl = url.parse(req.url, true);
@@ -79,54 +61,58 @@ const unifiedServer = function(req, res) {
   // Get the payload, if any
   const decoder = new StringDecoder('utf-8');
   let buffer = '';
-  req.on('data', function(data) {
+  req.on('data', function (data) {
     buffer += decoder.write(data);
   });
-  req.on('end', function() {
+  req.on('end', function () {
     buffer += decoder.end();
 
     // Log the request path
-    console.log(`Request received on path: ${trimmedPath} with method: ${method} and with these query string parameters: `)
+    /*console.log(`Request received on path: ${trimmedPath} with method: ${method} and with these query string parameters: `)
     console.log(queryStringObject)
     console.log('Request received with these headers')
     console.log(headers)
     console.log(`Request received with payload: ${buffer}`)
+*/
+    // Choose the handler the request should go to. If no one found choose notFound handler.
+    const chosenHandler = typeof (router[trimmedPath]) !== 'undefined'
+      ? router[trimmedPath]
+      : handlers.notFound;
+
+    // Construct data object to send to the handler
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      'payload': helpers.parseJsonToObject(buffer)
+    }
+
+    //  Route the request to the handler specified in the router
+    chosenHandler(data, function (statusCode, payload) {
+      //  Use the status code called back by the handler, or default to 200
+      statusCode = typeof (statusCode) === 'number'
+        ? statusCode
+        : 200;
+
+      // Use the payload called back by the handler, or default to empty object
+      payload = typeof (payload) === 'object'
+        ? payload
+        : {}
+
+      //  Convert the payload to a string
+      const payloadString = JSON.stringify(payload);
+
+      // Return the response
+      res.setHeader('Content-Type', 'application/json')
+      res.writeHead(statusCode);
+      res.end(payloadString);
+    });
   });
+}
 
-  // Choose the handler the request should go to. If no one found choose notFound handler.
-
-  const chosenHandler = typeof(router[trimmedPath]) !== 'undefined'
-    ? router[trimmedPath]
-    : handlers.notFound;
-
-  //  Construct data object to send to the handler
-  const data = {
-    trimmedPath,
-    queryStringObject,
-    method,
-    headers,
-    'payload': buffer
-  }
-
-  //  Route the request to the handler specified in the router
-  chosenHandler(data, function(statusCode, payload) {
-    //  Use the status code called back by the handler, or default to 200
-    statusCode = typeof(statusCode) === 'number'
-      ? statusCode
-      : 200;
-
-    // Use the payload called back by the handler, or default to empty object
-    payload = typeof(payload) === 'object'
-      ? payload
-      : {}
-
-    //  Convert the payload to a string
-    const payloadString = JSON.stringify(payload);
-
-    // Return the response
-    res.setHeader('Content-Type', 'application/json')
-    res.writeHead(statusCode);
-    res.end(payloadString);
-
-  })
+// Define a request router
+const router = {
+  'ping': handlers.ping,
+  'users': handlers.users
 }
